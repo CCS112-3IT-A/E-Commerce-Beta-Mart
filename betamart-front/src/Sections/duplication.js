23a6b047070c959687df718a7653e5029049fff3
Carl Lawrence M. Castillo <castillocarllawrencem.09@gmail.com>
2024-05-08 06:29:22 +0800

    update in duplication.js

--- betamart-front/src/Sections/duplication.js
+++ betamart-front/src/Sections/duplication.js
@@ -1,3 +1,13 @@
 // duplication.js
 const handleAddToCart = (productId, cartItems, updateQuantity, addToCart) => {
     const existingItemIndex = cartItems.findIndex(item => item.id === productId);
+
+    if (existingItemIndex !== -1) {
+        const existingItem = cartItems[existingItemIndex];
+        updateQuantity(existingItem, existingItem.quantity + 1);
+      } else {
+        addToCart(productId);
+      }
+    };
+    
+    export default handleAddToCart;
