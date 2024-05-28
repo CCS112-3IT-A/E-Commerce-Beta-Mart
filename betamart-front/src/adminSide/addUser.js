86b53d3fb1cec151e51a4270e5f13f57c66be21d
Carl Lawrence M. Castillo <castillocarllawrencem.09@gmail.com>
2024-05-29 05:56:30 +0800

    variables ready

--- betamart-front/src/adminSide/addUser.js
+++ betamart-front/src/adminSide/addUser.js
@@ -3,3 +3,14 @@ import { Modal, Button, Form, Row, Col, Alert } from 'react-bootstrap';
 
 const AddUser = ({ onAddUser }) => {
     const [show, setShow] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [retypePassword, setRetypePassword] = useState('');
+    const [error, setError] = useState('');
+
+  const handleClose = () => {
+    setShow(false);
+    setError('');
+  };
+
+  
