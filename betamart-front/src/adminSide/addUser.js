75f94ccfc0068b2b8556e8a8736ccd7b9869b0bf
Carl Lawrence M. Castillo <castillocarllawrencem.09@gmail.com>
2024-05-29 05:55:55 +0800

    still upadating the addUser

--- betamart-front/src/adminSide/addUser.js
+++ betamart-front/src/adminSide/addUser.js
@@ -1,2 +1,5 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form, Row, Col, Alert } from 'react-bootstrap';
+
+const AddUser = ({ onAddUser }) => {
+    const [show, setShow] = useState(false);
